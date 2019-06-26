71058d1e2b3e15f2380ac09836c2dd808f81e582
ngpaladi <ngpaladi@gmail.com>
2019-06-26 15:08:35 -0400

    Started Coldbox State Class

---
+++ ColdboxState.py
@@ -0,0 +1,6 @@
+import RemoteMultimeter as RM
+import time
+
+class ColdboxState:
+    def __init__(self, scan_result, ):
+        
--- RemoteMultimeter.py
+++ RemoteMultimeter.py
@@ -4,22 +4,23 @@ import time
 # Global Settings Declarations
 PRESSURE_SENSOR_SUPPLY_VOLTAGE = 8
 REFERENCE_PRESSURE = 1.013  # bar
-MAX_PRESSURE = 12  # bar
+MAX_PRESSURE = 59  # bar
 MIN_PRESSURE = -1  # bar
 
 
 class ScanResult:
     # Helper class for ease of reading returned values
 
-    def volatageToPressure(self, voltage_reading, supply_voltage):
+    def voltageToPressure(self, voltage_reading, supply_voltage):
 
         # Will convert voltage to pressure
         pressure = (voltage_reading - 0.1*supply_voltage)*(MAX_PRESSURE-MIN_PRESSURE)/(0.8*supply_voltage) + \
             MIN_PRESSURE+REFERENCE_PRESSURE  # Some linear function of voltage reading and supply voltage
         return pressure
 
-    def __init__(self, raw_result, num_temp, num_pres=0):
+    def __init__(self, raw_result, timestamp, num_temp, num_pres=0):
         self.raw_result = raw_result
+        self.timestamp = timestamp
 
         # Grab Temperatures
         self.temperatures = []
@@ -38,7 +39,6 @@ class ScanResult:
                     float(val[:-3]), PRESSURE_SENSOR_SUPPLY_VOLTAGE))
             i += 1
 
-
         # Grab Average Times
         self.timestamps = []
         i = 0
@@ -47,6 +47,9 @@ class ScanResult:
                 self.timestamps.append(float(val[:-4]))
             i += 1
         self.average_time = sum(self.timestamps)/len(self.timestamps)
+
+    def getCsvRow(self, start_time):
+        return str(self.average_time+self.timestamp-start_time)+","+','.join(map(str, self.temperatures+self.pressures))
 
     def __str__(self):
         return str(self.raw_result)
@@ -98,17 +101,16 @@ class RemoteMultimeter:
         # Save channel lists
         self.list_of_temp_channels = list_of_temp_channels
         self.list_of_volt_channels = list_of_volt_channels
-        
+
         # Create the total list of channels
-        list_of_channels = list_of_temp_channels
-        list_of_channels.extend(list_of_volt_channels)
+        list_of_channels = list_of_temp_channels + list_of_volt_channels
         self.list_of_channels = list_of_channels
 
         list_of_channels_str = ""
         for channel in list_of_channels:
             list_of_channels_str = list_of_channels_str+str(channel)+","
         list_of_channels_str = list_of_channels_str[:-1]
-
+        self.list_of_channels_str = list_of_channels_str
 
         # Start setup for Keithley 2700
         self.dev.write("TRAC:CLE")
@@ -119,14 +121,15 @@ class RemoteMultimeter:
         self.setupTemperatureChannels(self.list_of_temp_channels)
         self.setupVoltageChannels(self.list_of_volt_channels)
         self.dev.write("SAMP:COUN "+str(len(self.list_of_channels)))
-        self.dev.write("ROUT:SCAN (@"+list_of_channels_str+")")
+        self.dev.write("ROUT:SCAN (@"+self.list_of_channels_str+")")
 
         self.dev.write("ROUT:SCAN:TSO IMM")
         self.dev.write("ROUT:SCAN:LSEL INT")
 
     def scan(self):
+        timestamp = time.time_ns() / (10 ** 9)
         self.last_scan_result = ScanResult(
-            [x.strip() for x in self.dev.query("READ?").split(',')], len(self.list_of_temp_channels))
+            [x.strip() for x in self.dev.query("READ?").split(',')], timestamp, len(self.list_of_temp_channels))
         return self.last_scan_result
 
     def identify(self):
@@ -152,8 +155,8 @@ class RemoteMultimeter:
     def read(self, string):
         return self.dev.read(string)
 
-    def __str__(self) :
-        if self.connected :
+    def __str__(self):
+        if self.connected:
             return "Connected to device at "+str(self.ip)+":"+str(self.port)+"\n"+self.identify()
         else:
             return "Device at "+str(self.ip)+":"+str(self.port)+" is not yet connected"
