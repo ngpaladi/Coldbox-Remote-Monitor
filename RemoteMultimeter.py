53db01d678425243e601c918a5fbea65d7621463
ngpaladi <ngpaladi@gmail.com>
2019-06-26 11:02:59 -0400

    Implemented new scan result functionality

--- RemoteMultimeter.py
+++ RemoteMultimeter.py
@@ -1,38 +1,73 @@
 import visa
 import time
 
-#Global Settings Declarations
+# Global Settings Declarations
 PRESSURE_SENSOR_SUPPLY_VOLTAGE = 8
+REFERENCE_PRESSURE = 1.013  # bar
+MAX_PRESSURE = 12  # bar
+MIN_PRESSURE = -1  # bar
 
-# Helper class for ease of reading returned values
+
 class ScanResult:
-    def volatageToPressure(self, voltage_reading, supply_voltage):
-        #Will convert voltage to pressure 
-        pressure = 0 #Some linear function of voltage reading and supply voltage
-        return pressure
-    def __init__(self, raw_result, num_temp):
-        self.raw_result = raw_result
-        self.raw_result_list = [float(i) for i in raw_result.split(',')]
-        self.temperatures = raw_result_list[:num_temp]
-        self.pressures = []
-        for voltage in raw_result_list[num_temp:]:
-            self.pressures.append(self.volatageToPressure(voltage, PRESSURE_SENSOR_SUPPLY_VOLTAGE))
-        
+    # Helper class for ease of reading returned values
 
+    def volatageToPressure(self, voltage_reading, supply_voltage):
+
+        # Will convert voltage to pressure
+        pressure = (voltage_reading - 0.1*supply_voltage)*(MAX_PRESSURE-MIN_PRESSURE)/(0.8*supply_voltage) + \
+            MIN_PRESSURE+REFERENCE_PRESSURE  # Some linear function of voltage reading and supply voltage
+        return pressure
+
+    def __init__(self, raw_result, num_temp, num_pres=0):
+        self.raw_result = raw_result
+
+        # Grab Temperatures
+        self.temperatures = []
+        i = 0
+        for val in raw_result[:num_temp*3]:
+            if i % 3 == 0:
+                self.temperatures.append(float(val))
+            i += 1
+
+        # Grab Pressures
+        self.pressures = []
+        i = 0
+        for val in raw_result[:num_temp*3]:
+            if i % 3 == 0:
+                self.pressures.append(self.volatageToPressure(
+                    float(val), PRESSURE_SENSOR_SUPPLY_VOLTAGE))
+            i += 1
+
+
+        # Grab Average Times
+        self.timestamps = []
+        i = 0
+        for val in raw_result[:num_temp*3]:
+            if i % 3 == 1:
+                self.timestamps.append(float(val[:-4]))
+            i += 1
+        self.average_time = sum(self.timestamps)/len(self.timestamps)
+
+    def __str__(self):
+        return str(self.raw_result)
 
 
 class RemoteMultimeter:
+    # Class for interacting with the multimeter
+
     def __init__(self, ip, port):
-        #Set IP and Port
+        # Set IP and Port
         self.ip = ip
         self.port = port
+        self.connected = False
 
     def connect(self):
-        #Start visa resource manager
+        # Start visa resource manager
         self.rm = visa.ResourceManager("@py")
-        
-        #Open a socket connection to the Keithley 2701 device and configure read/write termination
-        self.dev = self.rm.open_resource('TCPIP::'+str(self.ip)+'::'+str(self.port)+'::SOCKET')
+
+        # Open a socket connection to the Keithley 2701 device and configure read/write termination
+        self.dev = self.rm.open_resource(
+            'TCPIP::'+str(self.ip)+'::'+str(self.port)+'::SOCKET')
         self.dev.read_termination = '\n'
         self.dev.write_termination = '\n'
 
@@ -41,27 +76,26 @@ class RemoteMultimeter:
         self.dev.write("*CLS")
         self.dev.write("TRAC:CLE")
 
-
-        #Setup display
+        # Setup display
         self.dev.write("DISP:TEXT:STAT ON")
         self.dev.write("DISP:TEXT:DATA 'READY'")
-                
+
+        self.connected = True
+
     def setupTemperatureChannels(self, list_of_channels):
-        #Sets each channel in the list for a thermistor
+        # Sets each channel in the list for a thermistor
         self.dev.write("UNIT:TEMP C")
         for channel in list_of_channels:
             self.dev.write("FUNC 'TEMP',(@"+str(channel)+")")
-            
 
     def setupVoltageChannels(self, list_of_channels):
-        #Sets each channel in the list
+        # Sets each channel in the list
         for channel in list_of_channels:
             self.dev.write("FUNC 'VOLT',(@"+str(channel)+")")
             self.dev.write("VOLT:RANG 10, (@"+str(channel)+")")
-        
 
     def setupChannels(self, list_of_temp_channels, list_of_volt_channels):
-        #Create the total list of channels
+        # Create the total list of channels
         list_of_channels = list_of_temp_channels
         list_of_channels.extend(list_of_volt_channels)
 
@@ -70,28 +104,28 @@ class RemoteMultimeter:
             list_of_channels_str = list_of_channels_str+str(channel)+","
         list_of_channels_str = list_of_channels_str[:-1]
 
-        #Save channel lists
+        # Save channel lists
         self.list_of_temp_channels = list_of_temp_channels
         self.list_of_volt_channels = list_of_volt_channels
         self.list_of_channels = list_of_channels
 
-        #Start setup for Keithley 2700
+        # Start setup for Keithley 2700
         self.dev.write("TRAC:CLE")
         self.dev.write("INIT:CONT OFF")
-        self.dev.write("TRIG:COUN INF")
+        self.dev.write("TRIG:COUN 1")
 
-        #List channels
+        # List channels
         self.setupTemperatureChannels(list_of_temp_channels)
         self.setupVoltageChannels(list_of_volt_channels)
         self.dev.write("SAMP:COUN "+str(len(list_of_channels)))
         self.dev.write("ROUT:SCAN (@"+list_of_channels_str+")")
-        
+
         self.dev.write("ROUT:SCAN:TSO IMM")
         self.dev.write("ROUT:SCAN:LSEL INT")
-        
 
     def scan(self):
-        self.last_scan_result = ScanResult((self.dev.query("READ?")),len(self.list_of_temp_channels))
+        self.last_scan_result = ScanResult(
+            (self.dev.query_ascii_values("READ?")), len(self.list_of_temp_channels))
         return self.last_scan_result
 
     def identify(self):
@@ -101,13 +135,13 @@ class RemoteMultimeter:
         self.dev.write("DISP:TEXT:DATA '"+string+"'")
 
     def disconnect(self):
-	#close the socket connection
+        # close the socket connection
         self.dev.write("DISP:TEXT:DATA 'CLOSING'")
         time.sleep(3)
-        self.dev.write("DISP:TEXT:STAT OFF")		
-        self.dev.write("ROUT:OPEN:ALL")	
+        self.dev.write("DISP:TEXT:STAT OFF")
+        self.dev.write("ROUT:OPEN:ALL")
 
-    #The following functions act as "pass-throughs" for SCPI commands
+    # The following functions act as "pass-throughs" for SCPI commands
     def write(self, string):
         return self.dev.write(string)
 
@@ -117,3 +151,8 @@ class RemoteMultimeter:
     def read(self, string):
         return self.dev.read(string)
 
+    def __str__(self) :
+        if self.connected :
+            return "Connected!\n"+self.identify()
+        else:
+            return "Device at "+str(self.ip)+":"+str(self.port)+" is not yet connected"
--- dmmReader.py
+++ dmmReader.py
@@ -4,10 +4,11 @@ import time
 dmm = RM.RemoteMultimeter("192.168.69.102",1394)
 dmm.connect()
 dmm.setupChannels([125],[126])
+print(dmm)
 print(dmm.scan())
 time.sleep(3)
+print(dmm.scan())
 dmm.display("LOADING")
 time.sleep(3)
-print(dmm.identify())
 dmm.disconnect()
 
